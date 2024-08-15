e26b18a731ab0bf9cf15a7560c782a06054779ab
Alza-3eem <176985194+Alza-3eem@users.noreply.github.com>
2024-08-15 13:28:50 -0400

    removed unnecessary JS

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
 const inputs = document.querySelectorAll("input");
-const password = document.getElementById("password")
-const confirmPw = document.getElementById("confirm")
-const form = document.getElementById("create-acct")
+const form = document.getElementById("create-acct");
 
 inputs.forEach(input => {
-  input.addEventListener(
-    "invalid",
-    event => {
-      input.classList.add("error");
-    },
-    false
-  );
+  input.addEventListener("invalid", event => {
+    event.target.classList.add("error");
+  }, false);
+
+  input.addEventListener("input", event => {
+    event.target.classList.remove("error");
+  }, false);
 });
-
-document.addEventListener("input", (e) => {
-	if (e.target == password) {
-		password.classList.add("pw-check")
-
-	}
-
-	if (e.target == password || e.target == confirmPw) {
-		if (confirmPw.value != password.value) {
-			confirmPw.classList.add("pw-confirm-check")	
-		} else {
-			confirmPw.classList.remove("pw-confirm-check")
-		}
-	}
-})
